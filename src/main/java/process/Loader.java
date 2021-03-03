13b52a5f28ffaa0a2056a155549779d52dc4e733
renanavs <ravs@ecomp.poli.br>
2021-03-03 14:54:15 -0300

    [FIX] - comparar instancias criadas dinâmicamente com as já persistidas na base de dados

--- src/main/java/process/Loader.java
+++ src/main/java/process/Loader.java
@@ -1,6 +1,9 @@
 package process;
 
+import model.Ambiente;
+import model.Cliente;
 import model.Fato;
+import model.Job;
 import org.hibernate.Transaction;
 
 import javax.persistence.EntityManager;
@@ -9,7 +12,7 @@ import java.util.List;
 
 public class Loader {
     private List<Fato> listFato;
-    private EntityManager entityManager;
+    private static EntityManager entityManager;
 
     public Loader(List<Fato> listFato, EntityManager entityManager) {
         this.listFato = listFato;
@@ -30,8 +33,9 @@ public class Loader {
             transaction.begin();
             for (Fato item :
                     listFato) {
-                this.entityManager.persist(item
-                );
+                merge(item);
+                System.out.println("Persistindo Fato id:"+item);
+                entityManager.persist(item);
             }
             transaction.commit();
         } catch (Exception e) {
@@ -39,5 +43,65 @@ public class Loader {
             return false;
         }
         return true;
+    }
+
+    private static Cliente findClienteInDatabase(Cliente obj) {
+        String sql = "FROM " + Cliente.class.getName();
+        sql += " WHERE descricao = :descricao ";
+
+        List<Cliente> result = entityManager.createQuery(sql, Cliente.class).
+                setParameter("descricao", obj.getDescricao()).
+                getResultList();
+
+        if(result.isEmpty()) {
+            return null;
+        }
+        return result.get(0);
+    }
+
+    private static Ambiente findAmbienteInDatabase(Ambiente obj) {
+        String sql = "FROM " + Ambiente.class.getName();
+        sql += " WHERE descricao = :descricao ";
+
+
+        List<Ambiente> result = entityManager.createQuery(sql, Ambiente.class).
+                setParameter("descricao", obj.getDescricao()).
+                getResultList();
+
+        if(result.isEmpty()) {
+            return null;
+        }
+        return result.get(0);
+    }
+
+    private static Job findJobInDatabase(Job obj) {
+        String sql = "FROM " + Job.class.getName();
+        sql += " WHERE descricao = :descricao ";
+
+
+        List<Job> result = entityManager.createQuery(sql, Job.class).
+                setParameter("descricao", obj.getDescricao()).
+                getResultList();
+
+        if(result.isEmpty()) {
+            return null;
+        }
+        return result.get(0);
+    }
+
+    private static void merge(Fato f) {
+        final Cliente cliente = findClienteInDatabase(f.getCliente());
+        final Ambiente ambiente = findAmbienteInDatabase(f.getAmbiente());
+        final Job job = findJobInDatabase(f.getJob());
+
+        if(cliente != null) {
+            f.setCliente(cliente);
+        }
+        if(ambiente != null) {
+            f.setAmbiente(ambiente);
+        }
+        if (job != null) {
+            f.setJob(job);
+        }
     }
 }
