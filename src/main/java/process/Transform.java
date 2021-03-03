daa1794678e509d5da48f9b2275c1fbca8084112
renanavs <ravs@ecomp.poli.br>
2021-03-03 12:14:53 -0300

    [FIX] - parseDate a partir dos datas das colunas da planilha definidos como texto

--- src/main/java/process/Transform.java
+++ src/main/java/process/Transform.java
@@ -7,6 +7,7 @@ import model.Job;
 import org.apache.poi.ss.usermodel.Row;
 
 import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,15 +34,22 @@ public class Transform {
             fato.setCliente(new Cliente(row.getCell(0).getStringCellValue()));
             fato.setAmbiente(new Ambiente(row.getCell(1).getStringCellValue()));
             fato.setJob(new Job(row.getCell(2).getStringCellValue()));
-            System.out.println(new Date((long) row.getCell(3).getNumericCellValue()));
-            /*fato.setDataExecucao(String.valueOf(row.getCell(3).getNumericCellValue()));
-            fato.setHoraInicio(new Time((long) row.getCell(3).getNumericCellValue()));
-            fato.setHoraFim(new Time((long)row.getCell(3).getNumericCellValue()));
-            fato.setTempoExecucao(new Time((long) row.getCell(3).getNumericCellValue()));
-            fato.setMetaAtualExecucao(new Time((long) row.getCell(3).getNumericCellValue()));*/
-            fatos.add(fato);
+            final String dataString = String.valueOf(row.getCell(3).getStringCellValue());
+            fato.setDataExecucao(parseDate(dataString));
+            //fato.setHoraInicio(new Time((long) row.getCell(3).getNumericCellValue()));
+            //fato.setHoraFim(new Time((long)row.getCell(3).getNumericCellValue()));
+            //fato.setTempoExecucao(new Time((long) row.getCell(3).getNumericCellValue()));
+            //fato.setMetaAtualExecucao(new Time((long) row.getCell(3).getNumericCellValue()));
+            //fatos.add(fato);
+            System.out.println("test");
         });
 
         return fatos;
+    }
+
+    private static Date parseDate(String date) {
+        final String dateTransform = date.substring(0,4) + "-" + date.substring(4, 6) + "-" + date.substring(6);
+
+        return Date.valueOf(dateTransform);
     }
 }
